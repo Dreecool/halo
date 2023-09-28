090337d31999fe5bb182cb1e746b095ef16e7d4f
Dreecool <114856837+Dreecool@users.noreply.github.com>
2023-09-28 16:36:24 +0800

    Update swiper.js

--- client/src/components/swiper.js
+++ client/src/components/swiper.js
@@ -5,7 +5,7 @@ import io from "socket.io-client";
 
 const SwiperComponent = ({ userID }) => {
 
-     const socket = io("https://halo-kappa.vercel.app");
+     const socket = io("wss://halo-kappa.vercel.app");
      const [onlineUsers, setOnlineUsers] = useState([]);
      
 
