e8a7c2c18787cfe3846f2b88097bb3f1192080e7
Dreecool <114856837+Dreecool@users.noreply.github.com>
2023-09-29 20:03:19 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ const socket = require("socket.io");
 const io = socket(server, {
 
   cors: {
-    origin: ["http://localhost:3000"]
+    origin: ["https://halo-78rf.vercel.app"],
+    methods: ["POST, GET, DELETE, PUT"],
+    credentials: true,
   }
 
 });
